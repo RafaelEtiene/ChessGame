9a1fe0e0b0959ef61474a6e9d76f63bc12223653
RafaelEtiene <rafaellimaetiene@gmail.com>
2023-04-17 20:16:29 -0300

    Primeiras movimentações das peças

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,14 +9,24 @@ namespace XadrezConsole
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
-                tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(2, 3));
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                
 
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+                
             }
             catch(TabuleiroException e)
             {
